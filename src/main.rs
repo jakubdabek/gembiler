c4ce9fc828aaff77bad96d44b27fb3bf2407ce76
Jakub Dąbek <jakub.dabek@gmail.com>
2020-01-07 00:29:00 +0100

    Add grammar file with test data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "program.pest"]
+struct ProgramParser;
+
+fn debug_file(filename: &str) {
+    let program_text = fs::read_to_string(filename).expect("unable to read file");
+    let program = ProgramParser::parse(Rule::program, &program_text);
+
+    println!("Result for {}: {:?}", filename, program);
+}
+
 fn main() {
-    println!("Hello, world!");
+    debug_file("test-data/program0.imp");
+    debug_file("test-data/program1.imp");
+    debug_file("test-data/program2.imp");
+
+    debug_file("test-data/p1.imp");
+
+    for i in 0..=8 {
+        debug_file(&format!("test-data/error{}.imp", i));
+    }
 }

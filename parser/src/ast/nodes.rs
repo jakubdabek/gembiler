74e5f00c2f20077d661d929bd279936143e59be2
Jakub Dąbek <jakub.dabek@gmail.com>
2020-01-10 17:33:05 +0100

    Add ast generation with tests

--- parser/src/ast/nodes.rs
+++ parser/src/ast/nodes.rs
@@ -1,57 +1,57 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Program {
     pub declarations: Option<Declarations>,
     pub commands: Commands,
 }
 
-type Declarations = Vec<Declaration>;
+pub type Declarations = Vec<Declaration>;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Declaration {
     Var { name: String },
     Array { name: String, start: i32, end: i32 },
 }
 
-type Commands = Vec<Command>;
+pub type Commands = Vec<Command>;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Command {
     IfElse { condition: Condition, positive: Commands, negative: Commands },
     If { condition: Condition, positive: Commands },
     While { condition: Condition, commands: Commands },
     Do { commands: Commands, condition: Condition },
-    For { counter: Identifier, ascending: bool, from: Value, to: Value },
+    For { counter: String, ascending: bool, from: Value, to: Value },
     Read { target: Identifier },
     Write { value: Value },
     Assign { target: Identifier, expr: Expression },
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum ExprOp { Plus, Minus, Times, Div, Mod, }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
     Simple { value: Value },
     Compound { left: Value, op: ExprOp, right: Value },
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum RelOp { EQ, NEQ, LEQ, LE, GEQ, GE, }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Condition {
     pub left: Value,
     pub op: RelOp,
     pub right: Value,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Value {
     Num(i32),
     Identifier(Identifier)
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Identifier {
     VarAccess { name: String },
     ArrAccess { name: String, index: String },
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,39 +2,397 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-mod ast;
+pub mod ast;
 
+use std::fs;
+use std::path::Path;
 use pest::Parser;
-use std::{fs, error::Error};
+use pest::iterators::Pairs;
+use crate::ast::*;
 
 #[derive(Parser)]
 #[grammar = "program.pest"]
 struct ProgramParser;
 
-pub fn debug_file(filename: &str) {
-    let program_text = fs::read_to_string(filename).expect("unable to read file");
-    let program = ProgramParser::parse(Rule::program, &program_text);
+type AstResult = Result<ast::Program, String>;
 
-    println!("Result for {}: {:?}", filename, program);
-}
-
-type AstResult = Result<ast::Program, Box<dyn Error>>;
-
-pub fn parse_file<P: AsRef<std::path::Path>>(path: P) -> AstResult {
-    let program_text = fs::read_to_string(path)?;
+pub fn parse_file<P: AsRef<Path>>(path: P) -> AstResult {
+    let program_text = fs::read_to_string(path).map_err(|e| e.to_string())?;
     parse_ast(&program_text)
 }
 
 pub fn parse_ast(text: &str) -> AstResult {
-    Ok(ast::Program { declarations: None, commands: vec![] })
+    let mut program: Pairs<Rule> = ProgramParser::parse(Rule::program, text).map_err(|e| e.to_string())?;
+
+    program = program.next().unwrap().into_inner().next().unwrap().into_inner();
+
+    let optional_declarations = program.next().unwrap();
+
+    let (declarations, commands) = match optional_declarations.as_rule() {
+        Rule::declarations => {
+            let pairs = optional_declarations.into_inner();
+            (Some(parse_declarations(pairs)), program.next().unwrap())
+        },
+        Rule::commands => (None, optional_declarations),
+        _ => unreachable!(),
+    };
+
+    let commands = parse_commands(commands.into_inner());
+
+    Ok(ast::Program { declarations, commands })
 }
+
+fn parse_declaration(mut pairs: Pairs<Rule>) -> Declaration {
+    let declaration = pairs.next().unwrap();
+    match declaration.as_rule() {
+        Rule::arr_decl => {
+            let mut parts = declaration.into_inner();
+            Declaration::Array {
+                name: parts.next().unwrap().as_str().to_owned(),
+                start: parts.next().unwrap().as_str().parse().unwrap(),
+                end: parts.next().unwrap().as_str().parse().unwrap(),
+            }
+        },
+        Rule::var_decl => Declaration::Var {
+            name: declaration.into_inner().next().unwrap().as_str().to_owned(),
+        },
+        _ => unreachable!(),
+    }
+}
+
+fn parse_declarations(pairs: Pairs<Rule>) -> Declarations {
+    pairs.map(|pair| parse_declaration(pair.into_inner())).collect()
+}
+
+fn parse_identifier(mut pairs: Pairs<Rule>) -> Identifier {
+    let name = pairs.next().unwrap().as_str().to_owned();
+
+    if let Some(index) = pairs.next() {
+        match index.as_rule() {
+            Rule::pidentifier => Identifier::ArrAccess {
+                name,
+                index: index.as_str().to_owned(),
+            },
+            Rule::num => Identifier::ArrConstAccess {
+                name,
+                index: index.as_str().parse().unwrap(),
+            },
+            _ => unreachable!(),
+        }
+    } else {
+        Identifier::VarAccess {
+            name,
+        }
+    }
+}
+
+fn parse_value(mut pairs: Pairs<Rule>) -> Value {
+    let value = pairs.next().unwrap();
+    match value.as_rule() {
+        Rule::num => Value::Num(value.as_str().parse().unwrap()),
+        Rule::identifier => Value::Identifier(parse_identifier(value.into_inner())),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_condition(mut pairs: Pairs<Rule>) -> Condition {
+    let left = parse_value(pairs.next().unwrap().into_inner());
+    let op = match pairs.next().unwrap().as_str() {
+        "EQ" => RelOp::EQ,
+        "NEQ" => RelOp::NEQ,
+        "LEQ" => RelOp::LEQ,
+        "LE" => RelOp::LE,
+        "GEQ" => RelOp::GEQ,
+        "GE" => RelOp::GE,
+        _ => unreachable!(),
+    };
+    let right = parse_value(pairs.next().unwrap().into_inner());
+
+    Condition {
+        left,
+        op,
+        right,
+    }
+}
+
+fn parse_expression(mut pairs: Pairs<Rule>) -> Expression {
+    let left = parse_value(pairs.next().unwrap().into_inner());
+
+    if let Some(op) = pairs.next() {
+        let op = match op.as_str() {
+            "PLUS" => ExprOp::Plus,
+            "MINUS" => ExprOp::Minus,
+            "TIMES" => ExprOp::Times,
+            "DIV" => ExprOp::Div,
+            "MOD" => ExprOp::Mod,
+            _ => unreachable!(),
+        };
+        let right = parse_value(pairs.next().unwrap().into_inner());
+
+        Expression::Compound {
+            left,
+            op,
+            right,
+        }
+    } else {
+        Expression::Simple {
+            value: left,
+        }
+    }
+}
+
+fn parse_ifelse(mut pairs: Pairs<Rule>) -> Command {
+    let condition = parse_condition(pairs.next().unwrap().into_inner());
+    let positive = parse_commands(pairs.next().unwrap().into_inner());
+    let negative = parse_commands(pairs.next().unwrap().into_inner());
+
+    Command::IfElse {
+        condition,
+        positive,
+        negative,
+    }
+}
+
+fn parse_conditional_command(mut pairs: Pairs<Rule>) -> (Condition, Commands) {
+    let condition = parse_condition(pairs.next().unwrap().into_inner());
+    let commands = parse_commands(pairs.next().unwrap().into_inner());
+
+    (condition, commands)
+}
+
+fn parse_if(pairs: Pairs<Rule>) -> Command {
+    let (condition, positive) = parse_conditional_command(pairs);
+
+    Command::If {
+        condition,
+        positive,
+    }
+}
+
+fn parse_while(pairs: Pairs<Rule>) -> Command {
+    let (condition, commands) = parse_conditional_command(pairs);
+
+    Command::While {
+        condition,
+        commands,
+    }
+}
+
+fn parse_do(pairs: Pairs<Rule>) -> Command {
+    let (condition, commands) = parse_conditional_command(pairs);
+
+    Command::Do {
+        condition,
+        commands,
+    }
+}
+
+fn parse_for(mut pairs: Pairs<Rule>) -> Command {
+    let counter = pairs.next().unwrap().as_str().to_owned();
+    let from = parse_value(pairs.next().unwrap().into_inner());
+    let ascending = match pairs.next().unwrap().as_str() {
+        "TO" => true,
+        "DOWNTO" => false,
+        _ => unreachable!(),
+    };
+    let to = parse_value(pairs.next().unwrap().into_inner());
+
+    Command::For {
+        counter,
+        from,
+        ascending,
+        to,
+    }
+}
+
+fn parse_read(mut pairs: Pairs<Rule>) -> Command {
+    let target = parse_identifier(pairs.next().unwrap().into_inner());
+
+    Command::Read {
+        target,
+    }
+}
+
+fn parse_write(mut pairs: Pairs<Rule>) -> Command {
+    let value = parse_value(pairs.next().unwrap().into_inner());
+
+    Command::Write {
+        value,
+    }
+}
+
+fn parse_assign(mut pairs: Pairs<Rule>) -> Command {
+    let target = parse_identifier(pairs.next().unwrap().into_inner());
+    let expr = parse_expression(pairs.next().unwrap().into_inner());
+
+    Command::Assign {
+        target,
+        expr,
+    }
+}
+
+fn parse_command(mut pairs: Pairs<Rule>) -> Command {
+    let command = pairs.next().unwrap();
+    match command.as_rule() {
+        Rule::cmd_ifelse => parse_ifelse(command.into_inner()),
+        Rule::cmd_if => parse_if(command.into_inner()),
+        Rule::cmd_while => parse_while(command.into_inner()),
+        Rule::cmd_do => parse_do(command.into_inner()),
+        Rule::cmd_for => parse_for(command.into_inner()),
+        Rule::cmd_read => parse_read(command.into_inner()),
+        Rule::cmd_write => parse_write(command.into_inner()),
+        Rule::cmd_assign => parse_assign(command.into_inner()),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_commands(pairs: Pairs<Rule>) -> Commands {
+    pairs.map(|pair| parse_command(pair.into_inner())).collect()
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(parse_ast("").unwrap(), ast::Program { declarations: None, commands: vec![] });
+    fn simplest() {
+        let text = "BEGIN WRITE 0; END";
+        let parsed = parse_ast(text);
+        let expected = ast::Program {
+            declarations: None,
+            commands: vec![
+                Command::Write { value: Value::Num(0), },
+            ],
+        };
+
+        assert_eq!(parsed.unwrap(), expected);
+    }
+
+    #[test]
+    fn simple_declarations() {
+        let text = r#"
+            DECLARE a, b, c(1:10)
+            BEGIN
+                WRITE 0;
+            END
+        "#;
+        let parsed = parse_ast(text);
+        let expected = ast::Program {
+            declarations: Some(vec![
+                Declaration::Var { name: String::from("a") },
+                Declaration::Var { name: String::from("b") },
+                Declaration::Array {
+                    name: String::from("c"),
+                    start: 1,
+                    end: 10,
+                },
+            ]),
+            commands: vec![
+                Command::Write { value: Value::Num(0), },
+            ],
+        };
+
+        assert_eq!(parsed.unwrap(), expected);
+    }
+
+    #[test]
+    fn program0() {
+        let text = r#"
+            [ binary representation ]
+            DECLARE
+                a, b
+            BEGIN
+                READ a;
+                IF a GEQ 0 THEN
+                    WHILE a GE 0 DO
+                        b ASSIGN a DIV 2;
+                        b ASSIGN 2 TIMES b; [ b := a & ~1 ]
+                        IF a GE b THEN
+                            WRITE 1;
+                        ELSE
+                            WRITE 0;
+                        ENDIF
+                        a ASSIGN a DIV 2;
+                    ENDWHILE
+                ENDIF
+            END
+        "#;
+
+        let parsed = parse_ast(text);
+
+        let var_a = Identifier::VarAccess { name: String::from("a") };
+        let var_b = Identifier::VarAccess { name: String::from("b") };
+
+        let expected = ast::Program {
+            declarations: Some(vec![
+                Declaration::Var { name: String::from("a") },
+                Declaration::Var { name: String::from("b") },
+            ]),
+            commands: vec![
+                Command::Read { target: var_a.clone() },
+                Command::If {
+                    condition: Condition {
+                        left: Value::Identifier(var_a.clone()),
+                        op: RelOp::GEQ,
+                        right: Value::Num(0),
+                    },
+                    positive: vec![
+                        Command::While {
+                            condition: Condition {
+                                left: Value::Identifier(var_a.clone()),
+                                op: RelOp::GE,
+                                right: Value::Num(0),
+                            },
+                            commands: vec![
+                                Command::Assign {
+                                    target: var_b.clone(),
+                                    expr: Expression::Compound {
+                                        left: Value::Identifier(var_a.clone()),
+                                        op: ExprOp::Div,
+                                        right: Value::Num(2),
+                                    }
+                                },
+                                Command::Assign {
+                                    target: var_b.clone(),
+                                    expr: Expression::Compound {
+                                        left: Value::Num(2),
+                                        op: ExprOp::Times,
+                                        right: Value::Identifier(var_b.clone()),
+                                    }
+                                },
+                                Command::IfElse {
+                                    condition: Condition {
+                                        left: Value::Identifier(var_a.clone()),
+                                        op: RelOp::GE,
+                                        right: Value::Identifier(var_b.clone()),
+                                    },
+                                    positive: vec![
+                                        Command::Write {
+                                            value: Value::Num(1),
+                                        }
+                                    ],
+                                    negative: vec![
+                                        Command::Write {
+                                            value: Value::Num(0),
+                                        }
+                                    ],
+                                },
+                                Command::Assign {
+                                    target: var_a.clone(),
+                                    expr: Expression::Compound {
+                                        left: Value::Identifier(var_a.clone()),
+                                        op: ExprOp::Div,
+                                        right: Value::Num(2),
+                                    }
+                                },
+                            ],
+                        }
+                    ],
+                }
+            ],
+        };
+
+        assert_eq!(parsed.unwrap(), expected);
     }
 }
